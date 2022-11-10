3cc42b00ceba4fa04b900d2037b51c45fb61aeef
Josh Sisskind <6191659+sisskind@users.noreply.github.com>
2022-11-10 13:49:37 -0500

    Removed semicolon

--- spark.js
+++ spark.js
@@ -6,7 +6,7 @@ function addPopUp(feature, layer){
 	if (feature.properties && feature.properties['@id']) {
 		// Excellent! We can now link directly to the feature
 		featureID = feature.properties['@id'];
-		osmURL = `https://www.openstreetmap.org/${featureID};`
+		osmURL = `https://www.openstreetmap.org/${featureID}`
 		popupTxt = `<b>OSM ${featureID.split('/')[0]} ID: ${featureID.split('/')[1]}</b>`;
 	} else if (feature.properties && feature.properties['id']) {
 		// Excellent! We can now link directly to the feature
